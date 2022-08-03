b0557f41ada196c8dc6a2300240b84202dadfb16
PhucDang <phuc.dang@zaitenllc.com>
2022-08-03 17:28:12 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+
+import { MenubarModule } from 'primeng/menubar';
+import { NavComponent } from './components/nav/nav.component';
+import { SlideshowComponent } from './components/slideshow/slideshow.component';
+import { GalleriaModule } from 'primeng/galleria';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { TabViewModule } from 'primeng/tabview';
+import { PhotoService } from './service/photoservice';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavComponent, SlideshowComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    RippleModule,
+    MenubarModule,
+    GalleriaModule,
+    TabMenuModule,
+    TabViewModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [PhotoService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
